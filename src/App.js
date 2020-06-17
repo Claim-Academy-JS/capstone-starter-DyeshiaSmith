cd449f70963d5ac96962313ab6e4ec523dafc0dd
D <dyeshiamonet@gmail.com>
2020-06-17 13:48:45 -0500

    rename files/folders to get ready for data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Home from "./components/Home";
 import Login from "./components/Login";
-import ReturnTool from "components/ReturnTool";
+import Return from "components/ReturnTool";
 import { NearMe } from "./components/BookTools/NearMe";
-import { Tools } from "./components/BookTools/Tools";
+import { Equipment } from "./components/BookTools/Equipment";
 
 export default class App extends React.Component {
   render() {
--- src/components/BookTools/Equipment.js
+++ src/components/BookTools/Equipment.js
@@ -1,7 +1,7 @@
 // list of tools OUTDOOR ITEMS- leaf blower, lawn mower, jack hammers, power washer, chain saws, water hose, sprinkler, gardening tools, tool box, drill set...HOUSEHOLD ITEMS hand tools, sewing machines, carpet cleaners, microwave, slow cooker, air fryer, pressure cooker, steamer, floor steamer, cricut machine, printer, laminator...AUTOMOTIVE - lift, jack, diagnostic kit, crow bar, jumper cables, air pumps, battery charger, jump starter
 import React from "react";
-export default class Tools extends React.Component {
+export default class Equipment extends React.Component {
   render() {
-    return <div>Tools</div>;
+    return <div>Equipment</div>;
   }
 }
