df716e7563f8059da3bede9d491387486789d2d6
Manav Misra <manavm@visionwebsoft.com>
2020-06-18 16:20:56 -0500

    :bug: Fix unknown issue in mongodb connection

--- backend/db/client.js
+++ backend/db/client.js
@@ -1,8 +1,13 @@
-import dotenv from "dotenv";
-import { MongoClient } from "mongodb";
+import dotenv from "dotenv"
+import { MongoClient } from "mongodb"
 
-dotenv.config();
+dotenv.config()
 
-export const client = new MongoClient(process.env.ATLAS_URI, {
-  useUnifiedTopology: true,
-});
+export const client = new MongoClient(
+  process.env.ATLAS_URI,
+  // TODO: Figure out what this is and why it's needed to turn off deprecation warning
+  {
+    useUnifiedTopology: true,
+  }
+)
+
--- backend/db/index.js
+++ backend/db/index.js
@@ -3,12 +3,11 @@ import { client } from "./client";
 export const getAllProducts = async () => {
   try {
     await client.connect();
-    const cursor = await client.db("equipped").collections("products").find();
-    console.log(cursor);
-    return await cursor.toArray();
-  } catch (err) {
-    throw new Error(err);
+    const cursor = await client.db("equipped").collection("products").find()
+    return await cursor.toArray()
+   } catch (err) {
+    throw new Error(err)
   } finally {
-    await client.close();
+    await client.close()
   }
-};
+}
