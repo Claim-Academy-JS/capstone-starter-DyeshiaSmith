c94ed08a2086134f596ffdf1dee6424f0efd407e
D <dyeshiamonet@gmail.com>
2020-06-22 16:08:08 -0500

    import normalize css and remove clutter from api

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,17 @@
-export const getAllProducts = async (endpoint) => {
-  const resp = await fetch("http://localhost:5000/api/products/products");
-  return resp.json();
-};
+// export const getAllProducts = async (endpoint) => {
+//   const resp = await fetch("http://localhost:5000/api/products/products");
+//   return resp.json();
+// };
 
-export const authenticateUser = async (authDeets) => {
-  const resp = await fetch("", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: JSON.stringify(authDeets),
-  });
-};
+// export const authenticateUser = async (authDeets) => {
+//   const resp = await fetch("", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/x-www-form-urlencoded",
+//     },
+//     body: JSON.stringify(authDeets),
+//   });
+//   console.log("posting", authDeets);
+// };
+
+export const makeRequest = async (endpoint, req) => await fetch(endpoint, req);
