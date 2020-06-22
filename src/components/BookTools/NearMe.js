c63c7c9379b9c3bf3f13b90c2732e75b67eebbf8
D <dyeshiamonet@gmail.com>
2020-06-22 16:13:07 -0500

    import future css files and render the props for table body

--- src/components/BookTools/NearMe.js
+++ src/components/BookTools/NearMe.js
@@ -1,29 +1,29 @@
-//Book Tools- Near Me(the Search)- use api to get location of people who log in from their computer.and the profile of the seller, rate they charge per hour, location...
-import PropTypes from "prop-types";
-import React from "react";
+// //Book Tools- Near Me(the Search)- use api to get location of people who log in from their computer.and the profile of the seller, rate they charge per hour, location...
+// import PropTypes from "prop-types";
+// import React from "react";
 
-import "NearMe.css";
+// import "NearMe.css";
 
-const renderTBody = (products) => {
-  return products.map(({ equipment, price, location }, index) => {
-    return (
-      <tr key={index}>
-        <td>{equipment}</td>
-        <td>{price}</td>
-        <td>{location}</td>
-      </tr>
-    );
-  });
-};
+// const renderTBody = (products) => {
+//   return products.map(({ equipment, price, location }, index) => {
+//     return (
+//       <tr key={index}>
+//         <td>{equipment}</td>
+//         <td>{price}</td>
+//         <td>{location}</td>
+//       </tr>
+//     );
+//   });
+// };
 
-export const ProductTable = ({ products }) => {
-  return (
+// export const ProductTable = ({ products }) => {
+//   return (
     <table>
-      <thead>
-        <tr>
-          <th>equipment</th>
-          <th>Price</th>
-          <th>location</th>
+//       <thead>
+//         <tr>
+//           <th>equipment</th>
+//           <th>Price</th>
+//           <th>location</th>
         </tr>
       </thead>
       <tbody>{renderTBody(products)}</tbody>
