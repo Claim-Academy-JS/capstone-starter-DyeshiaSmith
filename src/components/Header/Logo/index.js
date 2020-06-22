6f82a4aaef4ee7db9a8dc6701121a9035183ad9d
D <dyeshiamonet@gmail.com>
2020-06-22 16:28:05 -0500

    add sample image for my logo

---
+++ src/components/Header/Logo/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./logo.css";
+
+export const Logo = () => (
+  <figure>
+    <img src="https://unsplash.com/photos/Q5G6rsv373U" alt="Random Logo" />
+  </figure>
+);
