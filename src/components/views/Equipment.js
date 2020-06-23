8298d4fd85dc0466902afc76a1981f6bcb20af87
Manav Misra <manavm@visionwebsoft.com>
2020-06-23 16:09:43 -0500

    Use Equipment page to render table

--- src/components/views/Equipment.js
+++ src/components/views/Equipment.js
@@ -1,34 +1,12 @@
 import React from "react";
 
+import { FilterableProductTable as ProductTable } from "components/FilterableProductTable";
+
 export class Equipment extends React.Component {
-  state = {
-    products: [],
-  };
-
-  async componentDidMount() {
-    const res = await fetch("http://localhost:5000/api/products/products");
-    try {
-      this.setState({
-        products: await res.json(),
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  renderProducts = () =>
-    this.state.posts.map(({ equipment, location }, index) => (
-      <section key={index}>
-        <h2>{equipment}</h2>
-        <p>{location}</p>
-      </section>
-    ));
-
   render() {
     return (
       <main>
-        <h1>Blog</h1>
-        {this.renderProducts()}
+        <ProductTable />
       </main>
     );
   }
