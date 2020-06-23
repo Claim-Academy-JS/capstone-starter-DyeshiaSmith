4c43b4dd3580ac68a662c9fcf021edecf017c9ef
D <dyeshiamonet@gmail.com>
2020-06-23 13:55:32 -0500

    add footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,8 +4,9 @@ import styles from "./Footer.modules.css";
 export default class Footer extends React.Component {
   render() {
     return (
-      <div className={styles.footer}>
-        <i className="fas fa-envelope-open-text"></i>
+      <div className="footer">
+        blahhhhhhhhhh
+        <i src className="fas fa-envelope-open-text"></i>
         <i className="fas fa-phone"></i>
       </div>
     );
