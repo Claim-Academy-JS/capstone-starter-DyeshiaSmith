947be7d67afef5bd235972aad1e73146f8d0f55e
Manav Misra <manavm@visionwebsoft.com>
2020-06-24 08:02:49 -0500

    Restyle and reword About page to be consistent with other styles
    Add style module for about
    
    Preventing 'main' from interfering with other pages.

--- src/components/views/About/About.js
+++ src/components/views/About/About.js
@@ -1,34 +1,25 @@
 import React from "react";
-import "./About.css";
+
+import styles from "./About.module.css";
 
 export const About = () => (
-  <main className="pic">
-    <div className="text">
-      <h1>About Equipped</h1>
-      <section>
-        <h2>The Long</h2>
-        <p>
-          This company was founded in 2020 in the midst of a pandemic. it was
-          the perfect time for people to catch a break and spend time with
-          family. With all the free time people had Equipped is an online rental
-          service for all of your around the house
-          {/* tools you need. You a A company where that allows you to save and one
-          wants to waste money hiring a professional to do things your also
-          capable of. We are an online resource that allows you to rent unused
-          equipment of others near you. also while being able to renyour own
-          unused equipment for extra money. */}
-        </p>
-      </section>
-      <section>
-        <h2>The Short</h2>
-        <p>
-          ou a A company where that allows you to save and one wants to waste
-          money hiring a professional to do things your also capable of. We are
-          an online resource that allows you to rent unused equipment of others
-          near you. also while being able to renyour own unused equipment for
-          extra money
-        </p>
-      </section>
-    </div>
+  <main className={styles.main}>
+    <section>
+      <h2>The Long and Short of Equipped</h2>
+      <p>
+        This company was founded in 2020 in the midst of a pandemic. It was the
+        perfect time for people to catch a break and spend time with family.
+        With all of the free time people had Equipped is an online rental
+        service for all of your around the house.
+      </p>
+
+      <p>
+        A company where that allows you to save! No one wants to waste money
+        hiring a professional to do things that they can DIY just because they
+        are short a couple of tools. We are an online resource that allows you
+        to rent the unused equipment of others near you, while also while being
+        able to rent out your own unused equipment for extra money.
+      </p>
+    </section>
   </main>
 );
