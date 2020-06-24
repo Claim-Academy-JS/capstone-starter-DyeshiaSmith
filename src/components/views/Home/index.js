db3a6d7441d9f4f5106d02652779f295a2abbce9
Manav Misra <manavm@visionwebsoft.com>
2020-06-24 05:58:00 -0500

    :fire: Clean up unnecessary components

--- src/components/views/Home/index.js
+++ src/components/views/Home/index.js
@@ -2,15 +2,4 @@ import React from "react";
 
 import { Login } from "components/Forms/Login";
 
-import "./Home.css";
-
-export const Home = () => (
-  <main className="img">
-    <h1></h1>
-
-    <div className="hero">
-      <Login />
-    </div>
-    <section></section>
-  </main>
-);
+export const Home = () => <Login />;
